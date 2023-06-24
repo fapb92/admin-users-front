4b8907182ef7d38ac3af121c322d3a555775613f
Fabian Paredes <f.paredesb92@gmail.com>
2023-06-24 09:07:12 -0500

    Login page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Layout } from "../Components/Layouts/Layout";
+import { Forms } from "../Components/Layouts/Forms";
+import { InputEmail } from "../Components/Inputs/InputEmail";
+import { InputPassword } from "../Components/Inputs/InputPassword";
+import { SubmitBtn } from "../Components/Buttons/SubmitBtn";
+import { LinksBtn } from "../Components/Buttons/LinksBtn";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+	return (
+		<Layout title="Sign in">
+			<div>
+				<Forms title="Inicio de sesión">
+					<InputEmail name="Correo electrónico" />
+					<InputPassword name="Contraseña" />
+					<div className="flex justify-center">
+						<SubmitBtn text="Iniciar sesión" />
+						<LinksBtn color="red" text="Cancelar" to="/" />
+					</div>
+				</Forms>
+				<div className="flex justify-center">
+					<Link to="/resetpass" className='hover:bg-blue-700 hover:text-white p-2 rounded' children={"¿Olvido contraseña?"} />
+				</div>
+			</div>
+		</Layout>
+	);
+};
