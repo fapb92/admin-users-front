f97b61f25b9b72e9495e3e45deb9a341bc31e84c
Fabian Paredes <f.paredesb92@gmail.com>
2023-06-29 10:19:26 -0500

    se depura

--- src/Components/Tables/TableVerticalHead.jsx
+++ src/Components/Tables/TableVerticalHead.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export const TableVerticalHead = ({ data = {} }) => {
-	console.log(data);
 	const styleRows = "py-2 px-4 border-b border-gray-300";
 	return (
 		<table className="mt-4 bg-white w-1/2 border border-gray-300">
