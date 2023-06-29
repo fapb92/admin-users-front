46866e92992571e1f0d1880ca23ddf21c88375c8
Fabian Paredes <f.paredesb92@gmail.com>
2023-06-29 14:34:17 -0500

    funcion para verificar expiracion de token

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -4,5 +4,12 @@ export const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
 	const [auth, setAuth] = useState({});
-	return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
+	const session_is_expire = () => {
+		if (!auth?.session_expires_in) {
+			return true;
+		}
+		const time = auth?.session_expires_in - Date.now();
+		return time <= 0;
+	};
+	return <AuthContext.Provider value={{ auth, setAuth, session_is_expire }}>{children}</AuthContext.Provider>;
 }
