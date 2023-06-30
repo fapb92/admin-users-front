9858b643165cc978c2844307b9c182675188ab4e
Fabian Paredes <f.paredesb92@gmail.com>
2023-06-29 19:50:03 -0500

    links to home and select roles

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
 import { LogOut } from "./LogOut";
 import { useState } from "react";
 import { useAuth } from "../Hooks/useAuth";
+import { Link } from "react-router-dom";
 export const NavigationBar = () => {
 	const [showButtons, setShowButtons] = useState(false);
 	const { auth } = useAuth();
@@ -13,37 +13,35 @@ export const NavigationBar = () => {
 		<nav className="bg-gray-100 shadow-sm fixed inset-x-0 top-0 z-50">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center justify-between h-16">
-					<div></div>
-					<div className="flex items-center">
-						{showButtons && (
-							<div className="ml-4 flex items-center">
-								<LogOut />
-							</div>
-						)}
-						<button className="text-gray-800 hover:font-bold px-3 py-2 rounded-md text-sm" onClick={handleButtonClick}>
-							{auth?.user?.name || "Opciones"}
-						</button>
+					<div className="hidden md:block">
+						<div className="ml-10 flex items-baseline space-x-4">
+							<Link to="/" className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium">
+								Home
+							</Link>
+						</div>
 					</div>
-				</div>
-			</div>
-		</nav>
-	);
-
-	return (
-		<nav className="bg-gray-100">
-			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-				<div className="flex items-center justify-between h-16">
-					<div></div>
 					<div className="flex items-center">
 						{showButtons && (
-							<div className="ml-4 flex items-center">
-								<button className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Button 1</button>
-								<LogOut />
-							</div>
+							<>
+								<div className="ml-4 flex items-center">
+									<LogOut />
+								</div>
+								<div className="ml-4 flex items-center">
+									<Link
+										to="/profile/selectrole"
+										className="text-gray-800 px-3 py-2 rounded-md text-sm font-bold hover:bg-blue-500 hover:text-white"
+									>
+										Roles
+									</Link>
+								</div>
+							</>
 						)}
-						<button className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium" onClick={handleButtonClick}>
-							Username
-						</button>
+						<div className="flex flex-col items-center">
+							<button className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-bold" onClick={handleButtonClick}>
+								{auth?.user?.name || "Opciones"}
+								{auth?.role?.title ? <p className="text-gray-500 text-xs font-normal">{auth?.role?.title}</p> : null}
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
